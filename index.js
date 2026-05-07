a62008b6f3d4e99438a48516ceb660273089076b
riday-kumar <hridoykumarsaha07@gmail.com>
2026-05-08 00:08:05 +0600

    get parcel for certain id

--- index.js
+++ index.js
@@ -46,6 +46,14 @@ async function run() {
       res.send(allParcels);
     });
 
+    app.get("/parcels/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+
+      const result = await parcelCollection.findOne(query);
+      res.send(result);
+    });
+
     // parcel create api
     app.post("/parcels", async (req, res) => {
       const parcel = req.body;
